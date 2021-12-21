22534f30258e1ba7542a8d49a6a16d3b214cb445
jay146 <jayakrishna.jk14@gmail.com>
2021-12-21 20:28:13 +0530

    added lovescore generator

--- bmicalculator/bmi.js
+++ bmicalculator/bmi.js
@@ -1,7 +1,8 @@
 function bmiCalculator(weight,height){
      //var bmi= weight/(height*height);
      var bmi= weight/Math.pow(height,2);
- return  Math.round(bmi);
+ return  Math.round(bmi);ore
+ 
 }
   var bmi=bmiCalculator(65,1.8);
   console.log("bmi  value is "+bmi);
