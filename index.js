dd65f0bdfefbc12c8e55db483bbf04f60b327d59
jay146 <jayakrishna.jk14@gmail.com>
2021-12-23 13:18:02 +0530

    added drumkit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+document.querySelector("button").addEventListener("click",handleClick);
+//addEventListener() takes 2 parameters type(which we take as  Mouse Event (click)) 
+//and 2 parameter listner which is a js function called when click events detected.  
+function handleClick(){
+alert("i got clicked");
+}
